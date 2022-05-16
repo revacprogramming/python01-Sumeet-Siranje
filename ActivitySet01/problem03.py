4da7c8446efa112f14741676845f14fb2be03eda
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-05-16 07:15:56 +0000

    save

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,8 @@
 # Variables, Expressions & Statements
 
-hrs = float(input("Enter hours? "))
+#hrs = float(input("Enter hours? "
+hrs =float(input("Enter Hours:"))
+rate=float(input("enter rate:"))
+pay=hrs*rate
+print(pay)
+
--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,16 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+#hrs = input("Enter hours? ")
+hrs=float(input("input hours\n"))
+rate=float(input("enter rate\n"))
+
+if hrs<=40:
+    pay=hrs*rate  
+    print(pay)
+else:
+    ovrhrs=(hrs-40)
+    regular=rate*40
+    othrs=ovrhrs*(rate*1.5)
+    ovrpay=(regular+othrs)
+    print(ovrpay)
+ 
